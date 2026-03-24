684cce52ff3a5ab833c44f4c99dbd60ce20534b9
Jaden Cox <jaden@fieldpulse.com>
2026-03-24 17:39:29 -0500

    Auto-save

--- frontend/src/pages/RecipeDetailPage.tsx
+++ frontend/src/pages/RecipeDetailPage.tsx
@@ -362,7 +362,7 @@ export default function RecipeDetailPage() {
       </div>
 
       {/* Content */}
-      <div className="max-w-3xl mx-auto px-4 sm:px-6 pb-20 pt-8">
+      <div className="max-w-3xl mx-auto px-4 sm:px-6 pb-24 sm:pb-16 pt-8">
 
         {/* Info card */}
         <div
