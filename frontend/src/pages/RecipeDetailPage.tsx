468e65091bb083136f932b322ba1ca4a1a4d3607
Jaden Cox <jaden@fieldpulse.com>
2026-03-24 14:23:31 -0500

    Auto-save

--- frontend/src/pages/RecipeDetailPage.tsx
+++ frontend/src/pages/RecipeDetailPage.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { createPortal } from 'react-dom';
 import { useParams, Link, useNavigate } from 'react-router-dom';
 import { getRecipe, getPantryItems, addPantryItem, updatePantryItem, deleteRecipe, updateRecipe } from '../api';
 import type { Recipe, PantryItem } from '../types';
